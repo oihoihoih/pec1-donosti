20d2f820f0aff9b0be6b00b6a8b7c8c84e36aa45
oihoihoih <ochyuo@hotmail.com>
2025-10-31 10:58:49 +0100

    Reset with no-horizontal animations

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,9 +10,7 @@ gsap.registerPlugin(ScrollTrigger);
  * Write any other JavaScript below
  */
 
-// events: "onEnter onLeave onEnterBack onLeaveBack",
-// options: play, pause, resume, reset, restart, complete,
-
+// Animation of bouny elements
 gsap.utils.toArray(".bouncy").forEach((item) => {
   gsap.from(item, {
     y: 100,
@@ -28,3 +26,52 @@ gsap.utils.toArray(".bouncy").forEach((item) => {
     },
   });
 });
+
+// Animation of sections intro
+gsap.utils.toArray(".gsap-section-intro").forEach((section) => {
+  ScrollTrigger.create({
+    trigger: section,
+    start: "top top",
+    pin: true,
+    pinSpacing: false,
+    markers: false,
+  });
+});
+
+// Sun scaling animation
+ScrollTrigger.create({
+  trigger: "#sol .section-container",
+  start: "top bottom",
+  end: "top top",
+  scrub: true,
+  animation: gsap.fromTo(
+    ".sun",
+    {
+      scale: 1,
+      transformOrigin: "center center",
+    },
+    {
+      scale: 25,
+      ease: "power2.out",
+    }
+  ),
+});
+
+// Main-card entrance animation (from bottom to top)
+gsap.utils.toArray(".main-card").forEach((card) => {
+  gsap.from(card, {
+    y: 600,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: card,
+      start: "top 90%",
+      end: "top 70%",
+      toggleActions: "play none none reverse",
+    },
+  });
+});
+
+// events: "onEnter onLeave onEnterBack onLeaveBack",
+// options: play, pause, resume, reset, restart,
